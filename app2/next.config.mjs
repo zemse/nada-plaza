378ddbe615f45150cdb872459a311dd02bc46644
Soham Zemse <22412996+zemse@users.noreply.github.com>
2024-11-17 02:46:17 +0700

    ignore eslint

--- app2/next.config.mjs
+++ app2/next.config.mjs
@@ -1,6 +1,11 @@
 /* eslint-disable */
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   async headers() {
     return [
       {
