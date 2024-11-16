0e2cc116fa9e465069319f75920407dd253c41ba
SauravKanchan <sauravnk30@gmail.com>
2024-11-17 06:36:24 +0700

    fix UI

--- app2/app/nfc/components/WalletComponent.tsx
+++ app2/app/nfc/components/WalletComponent.tsx
@@ -8,7 +8,16 @@ export default function WalletComponent() {
       className="flex flex-col items-center justify-center space-y-6 w-full max-w-2xl mx-auto"
       style={{ color: "black" }}
     >
-      {address && <p>Address: {address}</p>}
+      {address && (
+        <div className="w-full text-center">
+          <p
+            className="bg-gray-100 text-gray-700 font-bold border border-gray-300 rounded-md py-2 px-4 mx-auto inline-block mt-4"
+            style={{ maxWidth: "90%" }}
+          >
+            Address: {address}
+          </p>
+        </div>
+      )}
       <p>
         {progress === 1 ? <p>Logging in...</p> : null}
         {progress === 2 ? <p>Logged in!</p> : null}
